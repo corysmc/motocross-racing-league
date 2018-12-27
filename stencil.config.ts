ed2e73c36fc78af41a090d7d62cd9320c3a8993f
corysmc <cory.s.mcarthur@gmail.com>
2018-12-27 07:56:11 -0700

    adding netlify config

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  copy: [{ src: '../netlify/_redirects', dest: '_redirects' }],
   outputTargets: [
     {
       type: 'www',
