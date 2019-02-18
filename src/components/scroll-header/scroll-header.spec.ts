6a6cfac0caf4115cb30dae9f4854141bcc00c7cc
corysmc <cory.s.mcarthur@gmail.com>
2019-02-18 15:11:43 -0700

    passing tests

--- src/components/scroll-header/scroll-header.spec.ts
+++ src/components/scroll-header/scroll-header.spec.ts
@@ -1,24 +1,7 @@
-import { TestWindow } from '@stencil/core/testing';
 import { ScrollHeader } from './scroll-header';
 
 describe('scroll-header', () => {
   it('should build', () => {
     expect(new ScrollHeader()).toBeTruthy();
-  });
-
-  describe('rendering', () => {
-    let element: HTMLScrollHeaderElement;
-    let testWindow: TestWindow;
-    beforeEach(async () => {
-      testWindow = new TestWindow();
-      element = await testWindow.load({
-        components: [ScrollHeader],
-        html: '<scroll-header></scroll-header>'
-      });
-    });
-
-    // See https://stenciljs.com/docs/unit-testing
-    {cursor}
-
   });
 });
